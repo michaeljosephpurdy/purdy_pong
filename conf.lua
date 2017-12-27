5ba7aeb12a492e281da26c45513b6edcd8dde0a8
michaeljosephpurdy <michaeljosephpurdy@gmail.com>
2017-12-27 16:24:38 -0500

    Main and conf files added

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(game)
+	game.console = true
+
+	game.window.title = "pong"
+	game.window.icon = nil
+	game.window.width = 400
+	game.window.height = 400
+end
---
+++ main.lua
@@ -0,0 +1,7 @@
+function love.load()
+end
+
+
+function love.draw()
+	love.graphics.print("Hello world!", 400, 400)
+end
