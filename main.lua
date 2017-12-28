e84f47ae9255f0a68055b88d72e5a685a9b145c1
michaeljosephpurdy <michaeljosephpurdy@gmail.com>
2017-12-27 22:34:28 -0500

    somewhat made improvements - trying to remember lua syntax

--- main.lua
+++ main.lua
@@ -1,7 +1,55 @@
+player = {};
+
 function love.load()
+  player.x = 20;
+  player.y = 20;
+  player.moveLeft = false;
+  player.moveRight = false;
+  player.moveUp = false;
+  player.moveDown = false;
 end
 
+function love.update(dt)
+  if player.moveLeft then
+    player.x = -player.x * dt;
+  end
+
+  if player.moveRight then
+    player.x = player.x * dt;
+  end
+
+  if player.moveDown then
+    player.y = -player.y * dt;
+  end
+
+  if player.moveUp then
+    player.y = -player.y * dt;
+  end
+
+  player.moveLeft = false;
+  player.moveRight = false;
+  player.moveUp = false;
+  player.moveDown = false;
+
+end
 
 function love.draw()
-	love.graphics.print("Hello world!", 400, 400)
+	love.graphics.print("Hello world!", player.x, player.y);
+end
+
+function love.keypressed(key)
+  player.moveDown = false;
+  if "a" == key then
+    player.moveLeft = true;
+
+  end
+  if "d" == key then
+    player.moveRight = true;
+  end
+  if "w" == key then
+    player.moveUp = true;
+  end
+  if "s" == key then
+    player.moveDown = true;
+  end
 end
