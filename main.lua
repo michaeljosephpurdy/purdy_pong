aa501ba958e8336ea123dbbd851528d4cdfcdf6f
michaeljosephpurdy <michaeljosephpurdy@gmail.com>
2017-12-28 18:10:09 -0500

    Added basic movement

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ player = {};
 function love.load()
   player.x = 20;
   player.y = 20;
+  player.speed = 20;
   player.moveLeft = false;
   player.moveRight = false;
   player.moveUp = false;
@@ -11,26 +12,20 @@ end
 
 function love.update(dt)
   if player.moveLeft then
-    player.x = -player.x * dt;
+    player.x = player.x - player.speed * dt;
   end
 
   if player.moveRight then
-    player.x = player.x * dt;
+    player.x = player.x + player.speed * dt;
   end
 
   if player.moveDown then
-    player.y = -player.y * dt;
+    player.y = player.y + player.speed * dt;
   end
 
   if player.moveUp then
-    player.y = -player.y * dt;
+    player.y = player.y - player.speed * dt;
   end
-
-  player.moveLeft = false;
-  player.moveRight = false;
-  player.moveUp = false;
-  player.moveDown = false;
-
 end
 
 function love.draw()
@@ -38,18 +33,37 @@ function love.draw()
 end
 
 function love.keypressed(key)
-  player.moveDown = false;
   if "a" == key then
     player.moveLeft = true;
-
   end
+
   if "d" == key then
     player.moveRight = true;
   end
+
   if "w" == key then
     player.moveUp = true;
   end
+
   if "s" == key then
     player.moveDown = true;
+  end
+end
+
+function love.keyreleased(key)
+  if "a" == key then
+    player.moveLeft = false;
+  end
+
+  if "d" == key then
+    player.moveRight = false;
+  end
+
+  if "w" == key then
+    player.moveUp = false;
+  end
+
+  if "s" == key then
+    player.moveDown = false;
   end
 end
