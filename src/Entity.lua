60a0f03ff3eba3a7b4c0df74a82bc20523bd746c
michaeljosephpurdy <michaeljosephpurdy@gmail.com>
2017-12-28 23:43:06 -0500

    Removed semicolons

--- src/Entity.lua
+++ src/Entity.lua
@@ -2,29 +2,29 @@ local class = require 'middleclass'
 Entity = class('Entity')
 
 function Entity:initialize(x, y)
-  self.x = x;
-  self.y = y;
-  self.speed = 20;
-  self.moveLeft = false;
-  self.moveRight = false;
-  self.moveUp = false;
-  self.moveDown = false;
+  self.x = x
+  self.y = y
+  self.speed = 20
+  self.moveLeft = false
+  self.moveRight = false
+  self.moveUp = false
+  self.moveDown = false
 end
 
 function Entity:update(dt)
   if self.moveLeft then
-    self.x = self.x - self.speed * dt;
+    self.x = self.x - self.speed * dt
   end
 
   if self.moveRight then
-    self.x = self.x + self.speed * dt;
+    self.x = self.x + self.speed * dt
   end
 
   if self.moveUp then
-    self.y = self.y - self.speed * dt;
+    self.y = self.y - self.speed * dt
   end
 
   if self.moveDown then
-    self.y = self.y + self.speed * dt;
+    self.y = self.y + self.speed * dt
   end
 end
--- src/Player.lua
+++ src/Player.lua
@@ -2,28 +2,28 @@ local class = require 'middleclass'
 Player = class('Player', 'Entity')
 
 function Player:initialize(x, y)
-  Entity.initialize(self, 20, 20);
+  Entity.initialize(self, 20, 20)
 end
 
 function Player:update(dt)
-  Entity.update(self, dt)
+  Entity.update(self, dt
 end
 
 function Player:pressedKeys(key)
   if "a" == key then
-    self.moveLeft = true;
+    self.moveLeft = true
   end
 
   if "d" == key then
-    self.moveRight = true;
+    self.moveRight = true
   end
 
   if "w" == key then
-    self.moveUp = true;
+    self.moveUp = true
   end
 
   if "s" == key then
-    self.moveDown = true;
+    self.moveDown = true
   end
 end
 
@@ -33,14 +33,14 @@ function Player:releasedKeys(key)
   end
 
   if "d" == key then
-    self.moveRight = false;
+    self.moveRight = false
   end
 
   if "w" == key then
-    self.moveUp = false;
+    self.moveUp = false
   end
 
   if "s" == key then
-    self.moveDown = false;
+    self.moveDown = false
   end
 end
