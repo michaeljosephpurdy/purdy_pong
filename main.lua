8f3c537b0f5a046a52708a46ff0802cb21862ce0
michaeljosephpurdy <michaeljosephpurdy@gmail.com>
2018-01-02 23:02:25 -0500

    Cleaned up input - added temporary touch interfacing

--- main.lua
+++ main.lua
@@ -1,20 +1,17 @@
 function love.load()
   Object = require "libraries.classic"
   require "src.Player"
-  require "src.Entity"
   
   redPlayer = Player("red", 20, 20, "left", "right")
   bluePlayer = Player("blue", 20, 330, "a", "d")
 end
 
 function love.update(dt)
-  Entities:update(dt)
   redPlayer:update(dt)
   bluePlayer:update(dt)
 end
 
 function love.draw()
-  Entities:draw()
   redPlayer:draw()
   bluePlayer:draw()
 end
--- src/Player.lua
+++ src/Player.lua
@@ -1,6 +1,8 @@
 Player = Object:extend()
 
 function Player:new(color, x, y, left, right)
+  self.windowWidth = love.graphics.getWidth()
+  self.windowHeight = love.graphics.getHeight()
   self:setColor(color)
   self.x = x
   self.y = y
@@ -20,9 +22,44 @@ function Player:setColor(color)
 end
 
 function Player:update(dt)
+  self:handleKeyboardInputs(dt)
+  self:handleTouchInputs(dt)
+end
+
+function Player:handleKeyboardInputs(dt)
   if love.keyboard.isDown(self.left) then
-    self.x = self.x - self.speed * dt
+    self:moveLeft(dt)
   elseif love.keyboard.isDown(self.right) then
+    self:moveRight(dt)
+  end
+end
+
+function Player:handleTouchInputs(dt)
+    local touches = love.touch.getTouches()
+    
+    for i, id in ipairs(touches) do
+      local x, y = love.touch.getPosition(id)
+      if x > 0 and x < self.windowWidth / 2
+        and y > 0 and y < self.windowHeight / 2 then
+          self:moveLeft(dt)
+      end
+      if x > self.windowWidth / 2 and x < self.windowWidth
+        and y > 0 and y < self.windowHeight / 2 then
+          self:moveRight(dt)
+      end
+        
+    end
+end
+
+function Player:moveLeft(dt)
+  if self.x >= 0 then
+    self.x = self.x - self.speed * dt
+  end
+end
+
+function Player:moveRight(dt)
+  if self.x + self.width <= self.windowWidth
+   then
     self.x = self.x + self.speed * dt
   end
 end
