270b4b3aee2528c1f5ea570194c5ccb152212900
michaeljosephpurdy <michaeljosephpurdy@gmail.com>
2018-01-05 22:20:22 -0500

    Modified updateAndPush script to remove love after pushing

--- updateAndPush.sh
+++ updateAndPush.sh
@@ -1,4 +1,4 @@
-zip -9 -r /tmp/game.love .
-adb push /tmp/game.love /sdcard/game.love
+zip -9 -r game.love .
+adb push game.love /sdcard/game.love
 adb shell am start -S -n "org.love2d.android/.GameActivity" -d "file:///sdcard/game.love"
-
+rm game.love
